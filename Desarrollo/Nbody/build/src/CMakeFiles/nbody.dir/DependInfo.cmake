93d6064b66235243f75734ea1a5fffc74371f4fd
ErnestoGuitar <47009643+ErnestoGuitar@users.noreply.github.com>
2023-03-17 16:58:13 -0500

    Add files via upload

---
+++ Desarrollo/Nbody/build/src/CMakeFiles/nbody.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/u185963/Examen_Final/oneAPI-samples/DirectProgramming/C++SYCL/N-BodyMethods/Nbody/src/GSimulation.cpp" "/home/u185963/Examen_Final/oneAPI-samples/DirectProgramming/C++SYCL/N-BodyMethods/Nbody/build/src/CMakeFiles/nbody.dir/GSimulation.cpp.o"
+  "/home/u185963/Examen_Final/oneAPI-samples/DirectProgramming/C++SYCL/N-BodyMethods/Nbody/src/main.cpp" "/home/u185963/Examen_Final/oneAPI-samples/DirectProgramming/C++SYCL/N-BodyMethods/Nbody/build/src/CMakeFiles/nbody.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
